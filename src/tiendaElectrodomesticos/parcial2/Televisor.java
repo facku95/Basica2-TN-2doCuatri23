c67eb58ca51c2158e891dd5bfd49c99c09bc098e
lautarocaputo <39624033+lautarocaputo@users.noreply.github.com>
2023-05-13 15:01:08 -0300

    Add files via upload

---
+++ src/tiendaElectrodomesticos/parcial2/Televisor.java
@@ -0,0 +1,44 @@
+package tiendaElectrodomesticos.parcial2;
+
+public class Televisor extends Electrodomestico {
+
+    private double tamaño;
+    private String resolucion;
+
+    public Televisor(int codigo, String nombre, double precio, int cantidadDisponible, String marca, String modelo, String tipo, double tamaño, String resolucion) {
+        super(codigo, nombre, precio, cantidadDisponible, marca, modelo, tipo);
+        this.tamaño = tamaño;
+        this.resolucion = resolucion;
+    }
+
+    @Override
+    public double calcularPrecioVenta(int cantidad) {
+        double precioVenta = getPrecio() * cantidad;
+        if (tamaño > 50) {
+            precioVenta *= 1.1; // aumentar el 10% del precio si es mayor a 50 pulgadas
+        }
+        return precioVenta;
+    }
+
+    public double getTamaño() {
+        return tamaño;
+    }
+
+    public void setTamaño(double tamaño) {
+        this.tamaño = tamaño;
+    }
+
+    public String getResolucion() {
+        return resolucion;
+    }
+
+    public void setResolucion(String resolucion) {
+        this.resolucion = resolucion;
+    }
+
+    @Override
+    public void registrarVenta(int cantidad) {
+        this.cantidadDisponible -= cantidad;
+        System.out.println("Se registro la venta de " + cantidad + " unidad(es) de " + this.getNombre() + ".");
+    }
+}
---
+++ src/tiendaElectrodomesticos/parcial2/Tienda.java
@@ -0,0 +1,81 @@
+package tiendaElectrodomesticos.parcial2;
+
+import java.util.List;
+
+import java.util.ArrayList;
+public class Tienda {
+
+    private List<Producto> productos;
+
+    public Tienda() {
+        this.productos = new ArrayList<>();
+    }
+
+    public void agregarProducto(Producto producto) {
+        this.productos.add(producto);
+    }
+
+    public void eliminarProducto(int codigo) {
+        Producto producto = buscarProducto(codigo);
+        if (producto != null) {
+            this.productos.remove(producto);
+        }
+    }
+
+    public Producto buscarProducto(int codigo) {
+        for (Producto producto : productos) {
+            if (producto.getCodigo() == codigo) {
+                return producto;
+            }
+        }
+        return null;
+    }
+
+    public void mostrarProductos() {
+        System.out.println("LISTA DE PRODUCTOS:");
+        System.out.println("====================");
+        for (Producto producto : productos) {
+            System.out.println("Codigo: " + producto.getCodigo());
+            System.out.println("Nombre: " + producto.getNombre());
+            System.out.println("Precio: $" + producto.getPrecio());
+            System.out.println("Cantidad disponible: " + producto.getCantidadDisponible());
+            if (producto instanceof Electrodomestico) {
+                Electrodomestico electrodomestico = (Electrodomestico) producto;
+                System.out.println("Marca: " + electrodomestico.getMarca());
+                System.out.println("Modelo: " + electrodomestico.getModelo());
+                System.out.println("Tipo: " + electrodomestico.getTipo());
+            }
+            if (producto instanceof Televisor) {
+                Televisor televisor = (Televisor) producto;
+                System.out.println("Tamaño: " + televisor.getTamaño() + " pulgadas");
+                System.out.println("Resolucion: " + televisor.getResolucion());
+            }
+            if (producto instanceof Refrigerador) {
+                Refrigerador refrigerador = (Refrigerador) producto;
+                System.out.println("Capacidad: " + refrigerador.getCapacidad() + " litros");
+                System.out.println("Tipo de refrigerador: " + refrigerador.getTipoRefrigerador());
+            }
+            System.out.println("====================");
+        }
+    }
+
+    public double calcularTotalVentas() {
+        double totalVentas = 0;
+        for (Producto producto : productos) {
+            totalVentas += producto.calcularPrecioVenta(producto.cantidadVendida);
+        }
+        return totalVentas;
+    }
+
+    public boolean registrarVenta(int codigo, int cantidad) {
+        Producto producto = buscarProducto(codigo);
+        if (producto != null && producto.getCantidadDisponible() >= cantidad) {
+            producto.registrarVenta(cantidad);
+            producto.cantidadVendida += cantidad;
+            return true;
+        } else {
+            System.out.println("No se pudo registrar la venta de " + cantidad + " unidad(es) de " + producto.getNombre() + ".");
+            return false;
+        }
+    }
+}
